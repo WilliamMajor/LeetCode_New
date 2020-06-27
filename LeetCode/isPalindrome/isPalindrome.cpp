b6346c9301e124a90565fc68e30d063d33d6587c
William Major <epiphone1995@gmail.com>
2020-06-26 17:39:26 -0700

    Optimized solution

--- LeetCode/isPalindrome/isPalindrome.cpp
+++ LeetCode/isPalindrome/isPalindrome.cpp
@@ -7,12 +7,15 @@ public:
         if (x < 0)
             return false;
 
-        while (testcase < storage)
+        for (int i = 0; i < 10; i++)
         {
             temp = x % 10;
             testcase = (10 * testcase) + temp;
-            if ((x /= 10) == 0)
+            x /= 10;
+            if (x == 0)
+            {
                 break;
+            }
         }
         return(testcase == storage);
     }
